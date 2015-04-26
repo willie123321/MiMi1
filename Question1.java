11eb6b55d2a510622ac8aa14d4a83a45b342a67c
JoeyC-NCU <joeychiou@minelab.csie.ncu.edu.tw>
2015-04-27 01:41:59 +0800

    Update q1

--- Question1.java
+++ Question1.java
@@ -1,7 +1,5 @@
 ﻿public class Question1{
 	public statis void main(String[] args){
-		txtCopy("Source.txt", "Dest.txt");
-		editFile("Dest.txt");
-		imgCopy("Source.jpg", "Dest.jpg");
+		System.out.println("Test");
 	}
 }
